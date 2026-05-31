bb334343d0fea0ae0219a7042e61ac4bc99fb4ea
om bante <ombante56.in>
2026-05-31 15:21:33 +0530

    texture change

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-            observer.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove('visible');
         }
     });
 }, {
